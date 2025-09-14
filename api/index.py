1c10645d29230240b84bfe51e795c22ad5f5e469
coma <s20021016@gmail.com>
2025-09-14 13:38:04 +0800

    test vercel API4

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 成功後可改白名單
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
